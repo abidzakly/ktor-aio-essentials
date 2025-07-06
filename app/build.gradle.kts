f3d74aaf22bff310147fd9b00af29fe3e16d6bc7
Amru Abid Zakly <amruabid8212@gmail.com>
2025-07-06 11:43:10 +0700

    feat: 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.abidzakly"
                 artifactId = "ktor-aio-essentials-library"
-                version = "2.0.0"
+                version = "2.0.1"
             }
         }
     }
